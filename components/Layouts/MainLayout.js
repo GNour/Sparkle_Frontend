e366fd8bbfe7a377b76e28592f64c5a6e9beffb3
Ghyath Noureddine <ghyathnour@gmail.com>
2021-10-17 18:17:32 +0300

    Install bootstrap@next package

--- components/Layouts/MainLayout.js
+++ components/Layouts/MainLayout.js
@@ -1,6 +1,13 @@
+import { Fragment } from "react";
+import UserCard from "../Common/Cards/UserCard/UserCard";
 import SideBar from "../Common/SideBar/SideBar";
 const MainLayout = () => {
-  return <SideBar />;
+  return (
+    <Fragment>
+      <SideBar />
+      <UserCard></UserCard>
+    </Fragment>
+  );
 };
 
 export default MainLayout;
