5f297dd0d7ff2e7a77685f29fcf02e1c322c53fe
Ghyath Noureddine <ghyathnour@gmail.com>
2021-10-18 20:27:13 +0300

    Add sample datatable

--- pages/teams/index.js
+++ pages/teams/index.js
@@ -1,9 +1,77 @@
 import { Fragment } from "react";
-import PageHeader from "../../components/Common/PageHeader";
+import PageHeaderWithActions from "../../components/Common/PageHeaderWithActions";
+import MUIDataTable from "mui-datatables";
 const TeamsPage = () => {
+  const columns = [
+    {
+      name: "Name",
+      options: {
+        sort: false,
+      },
+    },
+    {
+      name: "Leader",
+      options: {
+        sort: false,
+      },
+    },
+    {
+      name: "Description",
+      options: {
+        sort: false,
+      },
+    },
+    {
+      name: "Employees",
+      options: {
+        sort: true,
+      },
+    },
+  ];
+
+  const data = [
+    ["Joe James", "Test Corp", "Yonkers", 41],
+    ["John Walsh", "Test Corp", "Hartford", 5],
+    ["Bob Herm", "Test Corp", "Tampa", 6],
+    ["James Houston", "Test Corp", "Dallas", 8],
+    ["Joe James", "Test Corp", "Yonkers", 41],
+    ["John Walsh", "Test Corp", "Hartford", 5],
+    ["Joe James", "Test Corp", "Yonkers", 41],
+    ["John Walsh", "Test Corp", "Hartford", 5],
+    ["Bob Herm", "Test Corp", "Tampa", 6],
+    ["James Houston", "Test Corp", "Dallas", 8],
+    ["Joe James", "Test Corp", "Yonkers", 41],
+    ["John Walsh", "Test Corp", "Hartford", 5],
+    ["Joe James", "Test Corp", "Yonkers", 41],
+    ["John Walsh", "Test Corp", "Hartford", 5],
+    ["Bob Herm", "Test Corp", "Tampa", 6],
+    ["James Houston", "Test Corp", "Dallas", 8],
+    ["Joe James", "Test Corp", "Yonkers", 41],
+    ["John Walsh", "Test Corp", "Hartford", 5],
+    ["Joe James", "Test Corp", "Yonkers", 41],
+    ["John Walsh", "Test Corp", "Hartford", 5],
+    ["Bob Herm", "Test Corp", "Tampa", 6],
+    ["James Houston", "Test Corp", "Dallas", 8],
+    ["Joe James", "Test Corp", "Yonkers", 41],
+    ["John Walsh", "Test Corp", "Hartford", 5],
+  ];
+
+  const options = {
+    filter: false,
+    pagination: true,
+    search: false,
+    print: false,
+    download: false,
+  };
   return (
     <Fragment>
-      <PageHeader header="Teams" />
+      <PageHeaderWithActions header="Teams" />
+      <MUIDataTable
+        title={"All Teams"}
+        data={data}
+        columns={columns}
+        options={options}
+      />
     </Fragment>
   );
 };
