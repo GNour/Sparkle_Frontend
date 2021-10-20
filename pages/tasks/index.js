92c8700aa7562285eac4677cbde1d1063fc2f64c
Ghyath Noureddine <ghyathnour@gmail.com>
2021-10-20 22:52:25 +0300

    Fix styling and cards preview

--- pages/tasks/index.js
+++ pages/tasks/index.js
@@ -1,9 +1,55 @@
-import { Fragment } from "react";
-import PageHeaderWithActions from "../../components/Common/PageHeaderWithActions";
-import TasksLayoutAdmin from "../../components/Layouts/TasksLayout/TasksLayoutAdmin";
+import { Fragment, useState } from "react";
+import TasksLayout from "../../components/Layouts/TasksLayout/TasksLayout";
 import TaskCard from "../../components/Common/Cards/TaskCard/TaskCard";
+import ColoumnContainer from "../../components/Common/ColoumnContainer";
+import { Popover, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
 const TasksPage = () => {
-  const dummyTask = {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [popoverContent, setPopoverConter] = useState("Nothing to show");
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+  const dummyTaskAssigned = {
+    name: "Table Cleaning",
+    description: "Clean all tables on the first floor",
+    created_by: {
+      first_name: "John",
+      last_name: "Doe",
+      username: "johnny",
+    },
+    taskable: "Course",
+    status: 1,
+    taskable_id: 1,
+    completed: {
+      first_name: "complete",
+      last_name: "complete",
+      username: "johnny",
+    },
+    assignedto: {
+      first_name: "John",
+      last_name: "Doe",
+      username: "johnny",
+    },
+  };
+  const dummyTaskUnassigned = {
+    name: "Table Cleaning",
+    description: "Clean all tables on the first floor",
+    created_by: {
+      first_name: "John",
+      last_name: "Doe",
+      username: "johnny",
+    },
+    taskable: "Course",
+    status: 0,
+    taskable_id: 1,
+  };
+  const dummyTaskFinished = {
     name: "Table Cleaning",
     description: "Clean all tables on the first floor",
     created_by: {
@@ -17,14 +63,50 @@ const TasksPage = () => {
   };
   return (
     <Fragment>
-      <PageHeaderWithActions
-        header="Tasks"
-        haveNav={["All", "Courses", "Todos"]}
-        haveSubNav={["Assigned", "Unassinged", "Latest"]}
-      />
-      <TasksLayoutAdmin>
-        <TaskCard task={dummyTask} />
-      </TasksLayoutAdmin>
+      <TasksLayout>
+        <ColoumnContainer title="Assigned">
+          <TaskCard
+            task={dummyTaskAssigned}
+            handlePopoverContent={setPopoverConter}
+            handleClick={handleClick}
+          />
+          <TaskCard
+            task={dummyTaskAssigned}
+            handlePopoverContent={setPopoverConter}
+            handleClick={handleClick}
+          />
+          <TaskCard
+            task={dummyTaskAssigned}
+            handlePopoverContent={setPopoverConter}
+            handleClick={handleClick}
+          />
+        </ColoumnContainer>
+        <ColoumnContainer title="Unassigned">
+          <TaskCard task={dummyTaskUnassigned} />
+          <TaskCard task={dummyTaskUnassigned} />
+          <TaskCard task={dummyTaskUnassigned} />
+        </ColoumnContainer>
+        <ColoumnContainer title="Finished">
+          <TaskCard task={dummyTaskFinished} />
+          <TaskCard task={dummyTaskFinished} />
+          <TaskCard task={dummyTaskFinished} />
+        </ColoumnContainer>
+      </TasksLayout>
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <Typography className="p-3">{popoverContent}</Typography>
+      </Popover>
     </Fragment>
   );
 };
