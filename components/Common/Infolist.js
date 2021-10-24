8de30a6ece1838482e7d956d304bd16d416867e7
Ghyath Noureddine <ghyathnour@gmail.com>
2021-10-24 17:04:48 +0300

    Minor fixes in styling

--- components/Common/Infolist.js
+++ components/Common/Infolist.js
@@ -3,7 +3,7 @@ import IconText from "./IconText";
 const Infolist = ({ title, list, externalStyles }) => {
   return (
     <div className={externalStyles}>
-      <p className="mb-0">{title}</p>
+      <p className="mb-1">{title}</p>
       <div className="ms-2 fw-light">
         {list.map((item) => {
           return <IconText key={item.text} icon={item.icon} text={item.text} />;
