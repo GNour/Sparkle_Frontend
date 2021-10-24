dbe5af30236ea266f05e5b2a19392ff26bcd35ef
Ghyath Noureddine <ghyathnour@gmail.com>
2021-10-24 22:19:58 +0300

    Add condition to remove sidebar from */create pages

--- components/Layouts/MainLayout.js
+++ components/Layouts/MainLayout.js
@@ -1,9 +1,12 @@
 import SideBar from "../Common/SideBar/SideBar";
 import styles from "./MainLayout.module.scss";
+import { useRouter } from "next/router";
+import BackButton from "../Common/BackButton";
 const MainLayout = ({ children }) => {
+  const { asPath } = useRouter();
   return (
     <div className={`theme-default ${styles.MainLayout}`}>
-      <SideBar />
+      {asPath.endsWith("create") ? null : <SideBar />}
       <main className="px-lg-4 mt-5 py-5 py-md-0 px-md-4 w-100">
         {children}
       </main>
