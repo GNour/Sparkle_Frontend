e62197d9832ab6fc5689943bc27bdeef722eb1c9
Ghyath Noureddine <ghyathnour@gmail.com>
2021-10-28 00:11:50 +0300

    Add more props and fixes to acheive dynamic render for both staff and admins

--- components/TasksPage/TaskCard/TaskCard.js
+++ components/TasksPage/TaskCard/TaskCard.js
@@ -1,39 +1,125 @@
 import { Button } from "@material-ui/core";
-import { AiOutlinePlus, AiOutlineClose } from "react-icons/ai";
-import { BsPersonCheckFill } from "react-icons/bs";
+import { useEffect, useState } from "react";
+import { AiOutlinePlus, AiOutlineClose, AiOutlineCheck } from "react-icons/ai";
+import { BsPersonCheckFill, BsTrash } from "react-icons/bs";
 import { MdAssignmentInd } from "react-icons/md";
 import IconText from "../../Common/IconText";
 import InfoActionsFooter from "../../Common/InfoActionsFooter";
 import InfoBody from "../../Common/InfoBody";
 import styles from "./TaskCard.module.scss";
-const TaskCard = ({ task, handleClick, handlePopoverContent }) => {
-  return (
-    <div
-      className={`${styles.TaskCardContainer}`}
-      onClick={
-        handleClick
-          ? (event) => {
-              handlePopoverContent(task.completed.username);
-              handleClick(event);
-            }
-          : () => {}
-      }
-      style={{
-        borderImage: `linear-gradient(90deg, green ${25}%, rgba(0,0,0,0.2) 0%, rgba(0,0,0,0.2) 100%) 1`,
-      }}
-    >
-      <InfoBody
-        cornerText="deadline: 2020-01-01"
-        title="Clean Tables"
-        subTitle="@johnny"
-        body="Please clean all tables ASAP, and be carefull while going to store IT's SLIPPERY..."
-      />
-      {getCardFooter(task.status, task.id)}
-    </div>
-  );
+const TaskCard = ({
+  task,
+  handleActionButton,
+  handleRemoveAction,
+  handlePopoverContent,
+  userRole,
+}) => {
+  if (userRole == "Staff") {
+    return (
+      <div className={`${styles.TaskCardContainer}`}>
+        <InfoBody
+          cornerText={`${task.user_task.deadline}`}
+          title={task.name}
+          subTitle={`@${task.created_by.username}`}
+          body={`${task.description}`}
+        />
+        {getCardFooterEmployee(
+          task,
+          task.taskable_type,
+          task.id,
+          handleActionButton
+        )}
+      </div>
+    );
+  } else {
+    const taskStats = getTaskStats(task.users);
+    const taskStatsProgress = Math.floor(
+      (taskStats?.completedUsers.length / taskStats?.uncompletedUsers.length) *
+        100
+    );
+
+    return (
+      <div
+        className={`${styles.TaskCardContainer}`}
+        onClick={(e) => handlePopoverContent(e)}
+        style={{
+          borderImage: `linear-gradient(90deg, green ${
+            taskStatsProgress ? taskStatsProgress : 0
+          }%, rgba(0,0,0,0.2) 0%, rgba(0,0,0,0.2) 100%) 1`,
+        }}
+      >
+        <InfoBody
+          cornerText={`${
+            task.users.length != 0
+              ? `deadline: ${task.users[0].details.deadline}`
+              : ""
+          }`}
+          title={task.name}
+          subTitle={`@${task.created_by.username}`}
+          body={`${task.description}`}
+        />
+        {getCardFooterAdmin(
+          task,
+          task.assigned,
+          task.id,
+          handleActionButton,
+          handleRemoveAction,
+          taskStats
+        )}
+      </div>
+    );
+  }
+
+  return <div></div>;
 };
 
-const getCardFooter = (status, id) => {
+const getCardFooterEmployee = ({}, type, id, action) => {
+  if (type == "todo") {
+    return (
+      <InfoActionsFooter
+        actions={[
+          <Button
+            key={id}
+            id={id}
+            size="small"
+            variant="outlined"
+            startIcon={<AiOutlineCheck />}
+            onClick={action}
+          >
+            Complete
+          </Button>,
+        ]}
+      />
+    );
+  }
+  if (type == "course") {
+    return (
+      <InfoActionsFooter
+        actions={[
+          <Button
+            key={id}
+            id={id}
+            size="small"
+            variant="outlined"
+            startIcon={<AiOutlineCheck />}
+            onClick={action}
+          >
+            Start
+          </Button>,
+        ]}
+      />
+    );
+  }
+};
+
+const getCardFooterAdmin = (
+  task,
+  status,
+  id,
+  handleActionButton,
+  handleRemoveAction,
+  stats
+) => {
   if (status == 1) {
     return (
       <InfoActionsFooter
@@ -42,22 +128,23 @@ const getCardFooter = (status, id) => {
             key="completed"
             icon={<MdAssignmentInd />}
             style="me-2"
-            text="25"
+            text={stats?.completedUsers.length}
           />,
           <IconText
             key="uncompleted"
             icon={<BsPersonCheckFill />}
             style="me-2"
-            text="5"
+            text={stats?.uncompletedUsers.length}
           />,
         ]}
         actions={[
           <Button
-            key="1"
+            key={id}
+            id={id}
             size="small"
             variant="outlined"
             startIcon={<AiOutlineClose />}
-            onClick={"confirmUnassignModalOpen"}
+            onClick={handleActionButton}
           >
             Unassign
           </Button>,
@@ -69,10 +156,12 @@ const getCardFooter = (status, id) => {
       <InfoActionsFooter
         actions={[
           <Button
-            key="1"
+            key={id}
+            id={id}
             size="small"
             variant="outlined"
             startIcon={<AiOutlinePlus />}
+            onClick={handleActionButton}
           >
             Assign
           </Button>,
@@ -96,8 +185,19 @@ const getCardFooter = (status, id) => {
             size="small"
             variant="outlined"
             startIcon={<AiOutlinePlus />}
+            onClick={handleActionButton}
           >
             Reassign
+          </Button>,
+          <Button
+            key={id}
+            id={id}
+            size="small"
+            variant="outlined"
+            startIcon={<BsTrash />}
+            onClick={handleRemoveAction}
+          >
+            Remove
           </Button>,
         ]}
       />
@@ -105,4 +205,17 @@ const getCardFooter = (status, id) => {
   }
 };
 
+const getTaskStats = (users) => {
+  if (users.length == 0) return;
+  let completedUsers = [];
+  let uncompletedUsers = [];
+  users.forEach((user) => {
+    if (user.details.completed == 1) {
+      completedUsers.push(user.username);
+    } else {
+      uncompletedUsers.push(user.username);
+    }
+  });
+  return { completedUsers, uncompletedUsers };
+};
 export default TaskCard;
