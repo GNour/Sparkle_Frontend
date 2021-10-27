d40cca085a0adf9065f0c0b87fb6a55231cda16f
Ghyath Noureddine <ghyathnour@gmail.com>
2021-10-27 13:36:20 +0300

    Fetch user data

--- pages/employees/index.js
+++ pages/employees/index.js
@@ -10,30 +10,31 @@ import styles from "./Employees.module.scss";
 import ActionButtonWithIcon from "../../components/Common/Buttons/ActionButtonWithIcon";
 import { useRouter } from "next/router";
 import axiosConfig from "../../helpers/axiosConfig";
-import { useAuth } from "../../stores/AuthContext";
+import { ProtectRoute, useAuth } from "../../stores/AuthContext";
+import Cookies from "js-cookie";
+
 const MembersPage = (props) => {
   const { user } = useAuth();
   const router = useRouter();
-
+  const [fetchedEmployees, setFetchedEmployees] = useState([]);
   const handleToggleFilter = (value) => {
     toggledFilter ? setToggledFilter(false) : setToggledFilter(true);
   };
 
   useEffect(() => {
-    axiosConfig
-      .get(
-        "user/all",
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        }
-      )
-      .then((response) => {
-        console.log(response);
-      });
-  });
+    const getEmployees = async () => {
+      await axiosConfig
+        .get("user/all")
+        .then((response) => {
+          console.log(response.data);
+          setFetchedEmployees(response.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getEmployees();
+  }, []);
 
   const handleCreateEmployee = () => {
     console.log("Employee");
@@ -53,22 +54,24 @@ const MembersPage = (props) => {
         }
       />
       <AllEmployeesLayout>
-        <UserCard outerStyle="col" image={"/id_img.jpg"} imageText={"Waiter"} />
-        <UserCard outerStyle="col" image={"/id_img.jpg"} imageText={"Waiter"} />
-        <UserCard outerStyle="col" image={"/id_img.jpg"} imageText={"Waiter"} />
-        <UserCard outerStyle="col" image={"/id_img.jpg"} imageText={"Waiter"} />
-        <UserCard outerStyle="col" image={"/id_img.jpg"} imageText={"Waiter"} />
+        {fetchedEmployees.map((employee) => {
+          return (
+            <UserCard
+              key={employee.id}
+              id={employee.id}
+              image={`http://localhost:8000/images/default.png`}
+              imageText={employee.position}
+              action={() => router.push("/employees/" + employee.id)}
+              name={employee.first_name + " " + employee.last_name}
+              username={employee.username}
+              phone={employee.phone_number}
+              email={employee.email}
+            />
+          );
+        })}
       </AllEmployeesLayout>
     </Fragment>
   );
 };
 
 export default MembersPage;
-
-export async function getServerSideProps() {
-  return {
-    props: {
-      employees: {},
-    },
-  };
-}
