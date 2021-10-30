422d8bc4b974624012976d203bd02d92e1db8d78
Ghyath Noureddine <ghyathnour@gmail.com>
2021-10-30 16:03:49 +0300

    Link the create task modal to the backend

--- components/Layouts/TasksLayout/TasksLayout.js
+++ components/Layouts/TasksLayout/TasksLayout.js
@@ -8,28 +8,31 @@ import ActionButtonWithIcon from "../../Common/Buttons/ActionButtonWithIcon";
 import { AiOutlinePlus } from "react-icons/ai";
 import { Formik, Form } from "formik";
 import { useRouter } from "next/router";
+import axiosConfig from "../../../helpers/axiosConfig";
+import { useSWRConfig } from "swr";
 const TasksLayout = ({ children, taskableCourses }) => {
-  console.log(taskableCourses);
+  const { mutate } = useSWRConfig();
   const router = useRouter();
   const [isModalOpened, setIsModalOpen] = useState(false);
-  const [modalType, setModalType] = useState("");
 
   const handleOpen = (value) => {
-    setModalType(value);
     setIsModalOpen(true);
-    console.log(modalType);
   };
   const handleClose = () => {
     setIsModalOpen(false);
   };
 
-  const handleCreateCourse = () => {};
-
-  const handleCreateTask = (values, { setSubmitting }) => {
-    setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
-      setSubmitting(false);
-    }, 400);
+  const handleCreateTask = async (values, { setSubmitting }) => {
+    const res = await axiosConfig
+      .post("task/create", values)
+      .then(() => {
+        setSubmitting(false);
+        setIsModalOpen(false);
+        mutate("task/all");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -54,107 +57,83 @@ const TasksLayout = ({ children, taskableCourses }) => {
       />
       <div className="row g-3 gy-5 py-3 row-deck">{children}</div>
       <CustomModal isModalOpened={isModalOpened} modalClose={handleClose}>
-        <div className="modal-dialog modal-dialog-centered modal-md modal-dialog-scrollable">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h4>Create Task</h4>
-            </div>
-            <div className="modal-body">
-              <Formik
-                initialValues={{
-                  title: "",
-                  description: "",
-                  taskable_type: "todo",
-                  todo_title: "",
-                  todo_description: "",
-                  taskable_id: "",
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                  handleCreateTask(values, { setSubmitting });
-                }}
-              >
-                {(form) => {
-                  return (
-                    <Form>
-                      <TextInput
-                        key="title"
-                        placeholder="Task Title"
-                        label="Task Title"
-                        externalStyles="mb-3"
-                        name="title"
-                        type="text"
-                      />
+        <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-md">
+          <Formik
+            initialValues={{
+              name: "",
+              description: "",
+              taskable_type: "todo",
+              taskable_id: "",
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              handleCreateTask(values, { setSubmitting });
+            }}
+          >
+            {(form) => {
+              return (
+                <Form className="modal-content">
+                  <div className="modal-header">
+                    <h4>Create Task</h4>
+                  </div>
+                  <div className="modal-body">
+                    <TextInput
+                      key="title"
+                      placeholder="Task Title"
+                      label="Task Title"
+                      externalStyles="mb-3"
+                      name="name"
+                      type="text"
+                    />
 
-                      <TextAreaInput
-                        key="description"
-                        placeholder="Note Description"
-                        label="Note Description"
-                        externalStyles="mb-3"
-                        name="description"
-                        type="text"
-                      />
-                      <SelectInput
-                        label="Task type"
-                        externalStyles="mb-3"
-                        name="taskable_type"
-                      >
-                        <option value="todo" defaultValue>
-                          Todo
-                        </option>
-                        <option value="course">Course</option>
-                      </SelectInput>
-                      {form.values.taskable_type == "todo" ? (
-                        <div>
-                          <TextInput
-                            key="todo_title"
-                            placeholder="Todo Title"
-                            label="Todo Title"
-                            externalStyles="mb-3"
-                            name="todo_title"
-                            type="text"
-                          />
-
-                          <TextAreaInput
-                            key="todo_description"
-                            placeholder="Todo Description"
-                            label="Todo Description"
-                            externalStyles="mb-3"
-                            name="todo_description"
-                            type="text"
-                          />
-                        </div>
-                      ) : (
-                        <div>
-                          <SelectInput
-                            name="taskable_id"
-                            label="Select course"
-                            externalStyles="mb-3"
-                          >
-                            <option>Choose Course</option>
-                            {taskableCourses.map((course) => {
-                              return (
-                                <option key={course.id} value={course.id}>
-                                  {course.name}
-                                </option>
-                              );
-                            })}
-                          </SelectInput>
-                        </div>
-                      )}
-                      <div className="modal-footer">
-                        <ActionButtonWithIcon
-                          text="Close"
-                          isSecondary
-                          action={handleClose}
-                        />
-                        <ActionButtonWithIcon text="Create" />
+                    <TextAreaInput
+                      key="description"
+                      placeholder="Task Description"
+                      label="Task Description"
+                      externalStyles="mb-3"
+                      name="description"
+                      type="text"
+                    />
+                    <SelectInput
+                      label="Task type"
+                      externalStyles="mb-3"
+                      name="taskable_type"
+                    >
+                      <option value="todo" defaultValue>
+                        Todo
+                      </option>
+                      <option value="course">Course</option>
+                    </SelectInput>
+                    {form.values.taskable_type == "todo" ? null : (
+                      <div>
+                        <SelectInput
+                          name="taskable_id"
+                          label="Select course"
+                          externalStyles="mb-3"
+                        >
+                          <option>Choose Course</option>
+                          {taskableCourses.map((course) => {
+                            return (
+                              <option key={course.id} value={course.id}>
+                                {course.name}
+                              </option>
+                            );
+                          })}
+                        </SelectInput>
                       </div>
-                    </Form>
-                  );
-                }}
-              </Formik>
-            </div>
-          </div>
+                    )}
+                  </div>
+                  <div className="modal-footer">
+                    <ActionButtonWithIcon
+                      text="Close"
+                      isSecondary
+                      action={handleClose}
+                    />
+                    <ActionButtonWithIcon text="Create" buttonType="submit" />
+                  </div>
+                </Form>
+              );
+            }}
+          </Formik>
         </div>
       </CustomModal>
     </Fragment>
