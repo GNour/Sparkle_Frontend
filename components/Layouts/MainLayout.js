b88d8c75af4b9246fe94f562e9a58e895bb2391c
Ghyath Noureddine <ghyathnour@gmail.com>
2021-10-30 20:00:40 +0300

    Make sidebar fixed

--- components/Layouts/MainLayout.js
+++ components/Layouts/MainLayout.js
@@ -5,13 +5,16 @@ import BackButton from "../Common/BackButton";
 const MainLayout = ({ children }) => {
   const { asPath } = useRouter();
   return (
-    <div className={`theme-default ${styles.MainLayout}`}>
+    <div
+      className={`theme-default vh-100 ${styles.MainLayout} overflow-hidden`}
+    >
       {asPath.endsWith("create") || asPath.endsWith("login") ? null : (
         <div className={`${styles.MainLayoutSideBar}`}>
           <SideBar />
         </div>
       )}
-      <main className="px-lg-4 mt-5 py-5 py-md-0 px-md-4 w-100">
+
+      <main className="ps-lg-2 pe-lg-4 mt-2 w-100 py-5 py-md-0 vh-100 overflow-y-scroll pe-md-4 ps-md-2">
         {children}
       </main>
     </div>
