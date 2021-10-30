c3f74797376609446e067f63f6b5559cea164fbf
Ghyath Noureddine <ghyathnour@gmail.com>
2021-10-30 20:01:42 +0300

    Remove header and fix stylings

--- components/Layouts/ChatLayout.js
+++ components/Layouts/ChatLayout.js
@@ -3,7 +3,6 @@ import PageHeader from "../Common/PageHeader";
 const ChatLayout = ({ children }) => {
   return (
     <Fragment>
-      <PageHeader header="Global Chat" />
       <div className="row g-3 gy-5 py-3 row-deck">{children}</div>
     </Fragment>
   );
