cbda744181a09b56474afe95ac6dae468ded149b
Ghyath Noureddine <ghyathnour@gmail.com>
2021-10-31 22:39:53 +0200

    Add a subtitle prop used in course page

--- components/Common/PageHeader.js
+++ components/Common/PageHeader.js
@@ -1,8 +1,14 @@
 import styles from "./Common.module.scss";
-const PageHeader = ({ header }) => {
+const PageHeader = ({ header, subtitle }) => {
   return (
     <div>
-      <h3 className={`${styles.PageHeader} fw-bold py-2 mb-0`}>{header}</h3>
+      <div>
+        <h3 className={`${styles.PageHeader} fw-bold py-2 mb-0 d-inline-block`}>
+          {header}
+        </h3>
+        <h5 className="fw-light float-end">{subtitle}</h5>
+      </div>
+
       <hr />
     </div>
   );
