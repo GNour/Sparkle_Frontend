232c8424f501335a2b8acc2d694520821e1c5440
Ghyath Noureddine <ghyathnour@gmail.com>
2021-10-31 22:41:54 +0200

    Add grade subtitle to header

--- components/Layouts/TasksLayout/CourseLayout.js
+++ components/Layouts/TasksLayout/CourseLayout.js
@@ -1,11 +1,11 @@
 import { Fragment } from "react";
 import PageHeader from "../../Common/PageHeader";
 
-const CourseLayout = ({ children }) => {
+const CourseLayout = ({ children, title, grade }) => {
   return (
     <Fragment>
-      <PageHeader header="Course Title" />
-      <div className="row g-3 gy-5 py-3 row-deck">{children}</div>
+      <PageHeader header={title} subtitle={grade} />
+      <div className="row g-3 gy-5 py-3 w-100">{children}</div>
     </Fragment>
   );
 };
