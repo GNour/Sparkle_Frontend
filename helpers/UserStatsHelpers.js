486d6dfada855d826f9829c21b73ae42bba8c61a
Ghyath Noureddine <ghyathnour@gmail.com>
2021-11-01 19:54:01 +0200

    Add getCourseStats

--- helpers/UserStatsHelpers.js
+++ helpers/UserStatsHelpers.js
@@ -47,4 +47,22 @@ export const getNotesStats = (notes) => {
   return [negativeNotesCount, positiveNotesCount];
 };
 
-export const getCoursesStats = (courses) => {};
+export const getCoursesStats = (courses) => {
+  let gradeA = 0;
+  let gradeB = 0;
+  let gradeC = 0;
+  let gradeD = 0;
+  let gradeF = 0;
+
+  courses.forEach((course) => {
+    if (course.user_course) {
+      if (course.user_course.grade >= 90) gradeA++;
+      else if (course.user_course.grade >= 80) gradeB++;
+      else if (course.user_course.grade >= 70) gradeC++;
+      else if (course.user_course.grade >= 60) gradeD++;
+      else gradeF++;
+    }
+  });
+
+  return [gradeA, gradeB, gradeC, gradeD, gradeF];
+};
