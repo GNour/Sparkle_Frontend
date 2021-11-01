acfbe0568134814609783068ed14ad66177aa8a9
Ghyath Noureddine <ghyathnour@gmail.com>
2021-11-01 15:46:11 +0200

    Add router props to components

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import MainLayout from "../components/Layouts/MainLayout";
 import { ProtectRoute } from "../stores/AuthContext";
 import "../styles/globals.scss";
-import { AuthContextProvider, useAuth } from "../stores/AuthContext";
+import { AuthContextProvider } from "../stores/AuthContext";
 function MyApp({ Component, pageProps, router }) {
   return (
     <AuthContextProvider>
       <ProtectRoute router={router}>
         <MainLayout router={router}>
-          <Component {...pageProps} />
+          <Component router={router} {...pageProps} />
         </MainLayout>
       </ProtectRoute>
     </AuthContextProvider>
