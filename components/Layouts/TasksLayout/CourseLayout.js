d5b06295227a9890b49d552a4dd5445dd5f00c25
Ghyath Noureddine <ghyathnour@gmail.com>
2021-11-01 15:45:43 +0200

    Fix course layout

--- components/Layouts/TasksLayout/CourseLayout.js
+++ components/Layouts/TasksLayout/CourseLayout.js
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
+import BackButton from "../../Common/BackButton";
 import PageHeader from "../../Common/PageHeader";
 
 const CourseLayout = ({ children, title, grade }) => {
   return (
     <Fragment>
+      <BackButton />
       <PageHeader header={title} subtitle={grade} />
       <div className="row g-3 gy-5 py-3 w-100">{children}</div>
     </Fragment>
