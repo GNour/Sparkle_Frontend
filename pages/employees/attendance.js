4b04b7dee9e0f3ded748b414fc6fe14c581e3582
Ghyath Noureddine <ghyathnour@gmail.com>
2021-11-05 16:20:17 +0200

    Remove dummydata and getch data as server side props

--- pages/employees/attendance.js
+++ pages/employees/attendance.js
@@ -1,12 +1,8 @@
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import PageHeaderWithActions from "../../components/Common/PageHeaderWithActions";
 import MUIDataTable from "mui-datatables";
-import ActionButtonWithIcon from "../../components/Common/Buttons/ActionButtonWithIcon";
-import { AiOutlinePlus } from "react-icons/ai";
-import { createTeamModal } from "../../helpers/ModalHelper";
-import CustomModal from "../../components/Common/CustomModal";
 import axiosConfig from "../../helpers/axiosConfig";
-const AttendancePage = ({}) => {
+const AttendancePage = ({ attendance }) => {
   const columns = [
     {
       name: "Date",
@@ -40,125 +36,7 @@ const AttendancePage = ({}) => {
     },
   ];
 
-  const dummydata = [
-    {
-      id: 1,
-      user_id: 1,
-      status: 0,
-      created_at: "2021-11-01T19:04:29.000000Z",
-      updated_at: "2021-11-01T19:04:29.000000Z",
-      user: {
-        id: 1,
-        username: "admin",
-        first_name: "admin",
-        last_name: "admin",
-        email: "admin@gmail.com",
-        phone_number: "+961 78844775",
-        profile_picture: "imgs/gsdf/sdf.jpg",
-        gender: 0,
-        role: "Admin",
-        position: null,
-        created_at: "2021-11-01T16:25:18.000000Z",
-        updated_at: "2021-11-01T16:25:18.000000Z",
-        deleted_at: null,
-        team_id: null,
-      },
-    },
-    {
-      id: 2,
-      user_id: 1,
-      status: 0,
-      created_at: "2021-11-01T19:05:07.000000Z",
-      updated_at: "2021-11-01T19:05:07.000000Z",
-      user: {
-        id: 1,
-        username: "admin",
-        first_name: "admin",
-        last_name: "admin",
-        email: "admin@gmail.com",
-        phone_number: "+961 78844775",
-        profile_picture: "imgs/gsdf/sdf.jpg",
-        gender: 0,
-        role: "Admin",
-        position: null,
-        created_at: "2021-11-01T16:25:18.000000Z",
-        updated_at: "2021-11-01T16:25:18.000000Z",
-        deleted_at: null,
-        team_id: null,
-      },
-    },
-    {
-      id: 3,
-      user_id: 1,
-      status: 0,
-      created_at: "2021-11-01T19:05:13.000000Z",
-      updated_at: "2021-11-01T19:05:13.000000Z",
-      user: {
-        id: 1,
-        username: "admin",
-        first_name: "admin",
-        last_name: "admin",
-        email: "admin@gmail.com",
-        phone_number: "+961 78844775",
-        profile_picture: "imgs/gsdf/sdf.jpg",
-        gender: 0,
-        role: "Admin",
-        position: "Web Developer",
-        created_at: "2021-11-01T16:25:18.000000Z",
-        updated_at: "2021-11-01T16:25:18.000000Z",
-        deleted_at: null,
-        team_id: null,
-      },
-    },
-    {
-      id: 4,
-      user_id: 1,
-      status: 0,
-      created_at: "2021-11-01T19:13:52.000000Z",
-      updated_at: "2021-11-01T19:13:52.000000Z",
-      user: {
-        id: 1,
-        username: "admin",
-        first_name: "admin",
-        last_name: "admin",
-        email: "admin@gmail.com",
-        phone_number: "+961 78844775",
-        profile_picture: "imgs/gsdf/sdf.jpg",
-        gender: 0,
-        role: "Admin",
-        position: null,
-        created_at: "2021-11-01T16:25:18.000000Z",
-        updated_at: "2021-11-01T16:25:18.000000Z",
-        deleted_at: null,
-        team_id: null,
-      },
-    },
-    {
-      id: 5,
-      user_id: 1,
-      status: 0,
-      created_at: "2021-11-01T19:14:51.000000Z",
-      updated_at: "2021-11-01T19:14:51.000000Z",
-      user: {
-        id: 1,
-        username: "admin",
-        first_name: "admin",
-        last_name: "admin",
-        email: "admin@gmail.com",
-        phone_number: "+961 78844775",
-        profile_picture: "imgs/gsdf/sdf.jpg",
-        gender: 0,
-        role: "Admin",
-        position: null,
-        created_at: "2021-11-01T16:25:18.000000Z",
-        updated_at: "2021-11-01T16:25:18.000000Z",
-        deleted_at: null,
-        team_id: null,
-      },
-    },
-  ];
-
-  const data = populateAttendanceData(dummydata);
+  const data = populateAttendanceData(attendance);
   const options = {
     filter: false,
     pagination: true,
@@ -168,17 +46,6 @@ const AttendancePage = ({}) => {
     selectableRows: "none",
   };
 
-  const handleCreateTeam = async (values, { setSubmitting }) => {
-    await axiosConfig
-      .post("team/create", values)
-      .then((res) => {
-        setIsModalOpen(false);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    setSubmitting(false);
-  };
   return (
     <Fragment>
       <PageHeaderWithActions header="Attendance" />
@@ -208,18 +75,13 @@ const populateAttendanceData = (attendances) => {
 
 export const getServerSideProps = async () => {
   try {
-    const teams = await axiosConfig.post("server/teams", {
-      key: process.env.API_KEY,
-    });
-
-    const managers = await axiosConfig.post("server/managers", {
+    const attendance = await axiosConfig.post("iot/attendance", {
       key: process.env.API_KEY,
     });
 
     return {
       props: {
-        teams: teams.data,
-        managers: managers.data,
+        attendance: attendance.data,
       },
     };
   } catch (error) {
