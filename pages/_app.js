4a4b14012332c538346263e51321cf4387e241d7
Ghyath Noureddine <ghyathnour@gmail.com>
2021-11-05 16:20:35 +0200

    Subscribe to pusher channel here

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import MainLayout from "../components/Layouts/MainLayout";
 import { ProtectRoute } from "../stores/AuthContext";
 import "../styles/globals.scss";
-import { AuthContextProvider, useAuth } from "../stores/AuthContext";
+import { AuthContextProvider } from "../stores/AuthContext";
 import { useEffect } from "react";
 import Pusher from "pusher-js";
 import { mutate } from "swr";
@@ -9,10 +9,8 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 function MyApp({ Component, pageProps, router }) {
-  const { user } = useAuth();
-  console.log(user);
   useEffect(() => {
-    Pusher.logToConsole = true;
+    Pusher.logToConsole = false;
 
     var pusher = new Pusher("31ab41bd72b4edd636e8", {
       cluster: "ap2",
@@ -20,8 +18,6 @@ function MyApp({ Component, pageProps, router }) {
 
     var channel = pusher.subscribe("chat");
     channel.bind("message", function (data) {
-      console.log(data);
-
       // FROM ARDUINO
       if (data.from == -1) {
         toast(data.message);
